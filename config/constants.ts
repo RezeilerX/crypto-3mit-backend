b2bbcab6607ecaba5b64f281ad5eb3f28e31ed08
Eliezer Peña <52340161+EliezerSPP@users.noreply.github.com>
2022-05-10 03:50:17 -0400

    feat: Added JWT_SECRET variable

--- config/constants.ts
+++ config/constants.ts
@@ -7,3 +7,4 @@ export const PORT = env.PORT ?? 80
 export const BASE_URL = `${PROTOCOL}://${HOSTNAME}:${PORT}`
 
 export const DB_URI = env.DB_URI ?? ''
+export const JWT_SECRET = env.JWT_SECRET ?? ''
