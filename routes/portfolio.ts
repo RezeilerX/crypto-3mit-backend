85e33cd28ac29e14e19e50afbdd293d9a165eb62
Eliezer Peña <52340161+EliezerSPP@users.noreply.github.com>
2022-05-10 23:48:49 -0400

    fix: Remove portfolio validator from controller and add authentication middleware

--- routes/portfolio.ts
+++ routes/portfolio.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { portfolioValidators } from 'validators/portfolio'
+import authMiddleware from 'middlewares/authMiddleware'
 import { getPortfolioController } from 'controllers/portfolio'
 
 const router = Router()
 
 // Portfolio detail
-router.get('/', portfolioValidators, getPortfolioController)
+router.get('/', authMiddleware, getPortfolioController)
 
 export default router
