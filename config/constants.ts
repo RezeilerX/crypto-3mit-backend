e850cbe40631e1c8d95d803c2244689668aa7fe3
Eliezer Peña <52340161+EliezerSPP@users.noreply.github.com>
2022-05-11 08:27:50 -0400

    refactor: Remove unused config constants

--- config/constants.ts
+++ config/constants.ts
@@ -1,10 +1,5 @@
 const env = process.env
 
-export const PROTOCOL = env.PROTOCOL ?? 'http'
-export const HOSTNAME = env.HOSTNAME ?? 'localhost'
 export const PORT = env.PORT ?? 80
-
-export const BASE_URL = `${PROTOCOL}://${HOSTNAME}:${PORT}`
-
 export const DB_URI = env.DB_URI ?? ''
 export const JWT_SECRET = env.JWT_SECRET ?? ''
